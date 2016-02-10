325d0985ada56b728658c634494942c0c7ef3140
Alejandro Reyna <aleejandroreyna@gmail.com>
2016-02-10 16:44:54 -0600

    prevention finished

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react"
+import PreventiveButton from "./button"
+
+function action(component) {
+    console.log(component);	
+    setTimeout(function() {
+    actionFinished(component)
+  }, 2000);
+}
+
+function actionFinished(component) {
+  console.log("done");
+  component.reset();
+}
 
 export default class App extends Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <PreventiveButton action={action} label="Submit" />	
+    ) 
   }
 }
---
+++ src/components/button.js
@@ -0,0 +1,39 @@
+import React, { Component } from "React";
+import { Button } from "react-bootstrap";
+
+export default class PreventiveButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {disabled: false};
+    this.enable = this.enable.bind(this);
+    this.disable = this.disable.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  enable() {
+    this.setState({disabled: false});
+  }  
+
+  disable() {
+    this.setState({disabled: true });
+  }
+
+  handleButtonClick() {
+    this.disable();
+    this.props.action(this);
+  }
+
+  reset(){
+    this.enable();
+  }
+  render() {
+    let{disabled} = this.state; 
+    let { label } = this.props;
+    let text = (disabled) ? "Submitting..." : label;
+    return (
+      <Button bsStyle="primary" disabled={disabled} onClick={this.handleButtonClick} >{text}</Button>
+    )
+  }
+} 
+
